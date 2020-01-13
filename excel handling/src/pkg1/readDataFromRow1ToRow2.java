0fa22a43dc8f9a266cd267c463014b5307ee8444
admin <admin@admin-HP>
2020-01-13 16:37:54 +0530

    updated

--- excel handling/src/pkg1/readDataFromRow1ToRow2.java
+++ excel handling/src/pkg1/readDataFromRow1ToRow2.java
@@ -2,6 +2,7 @@ package pkg1;
 //READ DATA FROM ROW1 TO ROW2
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -15,22 +16,29 @@ public class readDataFromRow1ToRow2
 		File f=new File("C:\\Users\\admin\\Desktop\\file1.xls");
 		Workbook wk=Workbook.getWorkbook(f);
 		Sheet ws=wk.getSheet(0);
-		row1=ws.getRows();
-		row2=ws.getRows();
-		
-		for(int i=0;i<row1;i++)
+		//row1=ws.getRows();
+		//row2=ws.getRows();
+		int c1=ws.getColumns();
+		for(int i=0;i<row2;i++)
 		{
-			for(int j=0;j<row2;j++)
+			if((i>=row1)&(i<=row2))
+			{
+				for(int j=0;j<c1;j++)
 			{
 				Cell c=ws.getCell(j,i);
 				System.out.println(c.getContents());
+			}
 			}
 		}
 	}
 	public static void main(String[] args) throws BiffException, IOException 
 	{
 		readDataFromRow1ToRow2 obj=new readDataFromRow1ToRow2();
-		obj.readdataofrow(5,3);
+		Scanner s=new Scanner(System.in);
+		 System.out.println("ENTER ROW ");
+		 int r=s.nextInt();
+		 int r1=s.nextInt();
+		obj.readdataofrow(r,r1);
 		
 	}
 
